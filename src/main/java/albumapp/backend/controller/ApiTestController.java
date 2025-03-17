566bb686963a333e1c6ea281d126bf484226b885
anrlll <anriii.3939@gmail.com>
2025-03-17 21:48:13 +0900

    first commit

---
+++ src/main/java/albumapp/backend/controller/ApiTestController.java
@@ -0,0 +1,14 @@
+package albumapp.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiTestController {
+    @GetMapping("/test")
+    public String testMessage(){
+        return "Connect";
+    }
+}
