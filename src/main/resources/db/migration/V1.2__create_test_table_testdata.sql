f607454d0e90be4b313dea0fd81c51574a6e3c14
anrlll <anriii.3939@gmail.com>
2025-03-21 02:19:19 +0900

    flyway導入　postgresバージョン変更

---
+++ src/main/resources/db/migration/V1.2__create_test_table_testdata.sql
@@ -0,0 +1,6 @@
+INSERT INTO test (message, sub_message) VALUES
+    ('Hello, World!', 'This is a test message.')
+    , ('PostgreSQL Insert Test', 'Testing sub_message column.')
+    , ('Sample Data', NULL)
+    , ('Another Message', 'With a sub message')
+    , ('Final Entry', NULL);
