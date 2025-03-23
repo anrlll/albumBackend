f3e35c43cbed715c68fa4849bd18202b28d371ae
anrlll <anriii.3939@gmail.com>
2025-03-23 14:57:03 +0900

    TODO追記

--- src/main/java/albumapp/backend/service/account/AccountService.java
+++ src/main/java/albumapp/backend/service/account/AccountService.java
@@ -3,6 +3,7 @@ package albumapp.backend.service.account;
 import albumapp.backend.controller.account.dto.SignUpDto;
 import albumapp.backend.domain.model.account.AccountModel;
 import albumapp.backend.domain.repository.account.AccountRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
@@ -10,13 +11,15 @@ import java.net.URI;
 @Service
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public AccountService(AccountRepository accountRepository) {
+    public AccountService(AccountRepository accountRepository, BCryptPasswordEncoder passwordEncoder) {
         this.accountRepository = accountRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public URI registerUser(SignUpDto dto){
-        //TODO PWを暗号化する
+        //TODO PWを暗号化する security周りが関わってくるので認証のところで一緒にやる
         var userModel = AccountModel.generalUser(
                 dto.userName()
                 , dto.email()
